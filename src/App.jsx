26fbd9200213a79da26f877040d28aa1ce697f6c
bartlomiejra <bartlomiej.ra@gmail.com>
2024-09-27 18:18:03 +0200

    small updates, colors, loading,

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import rumbleVideosData from './assets/data/rumble_videos.json';
 import youtubeVideosData from './assets/data/youtube_videos.json';
 import podcastsData from './assets/data/podcasts.json';
 import VideoCount from './VideoCount';
-import rumbleLogo from './assets/rumble.png'; 
+import rumbleLogo from './assets/rumble.png';
+import podcastImage from './assets/podcast.png'; // Import the podcast image
 import './index.css';
 
 function App() {
   const [videos, setVideos] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
   const [selectedVideoId, setSelectedVideoId] = useState('');
   const [selectedPlatform, setSelectedPlatform] = useState('youtube'); // Domyślnie ustawione na YouTube
-  const [loading, setLoading] = useState(true); // Stan ładowania
+  const [loading, setLoading] = useState(false); // Stan ładowania
 
   useEffect(() => {
     // Łączymy dane z wszystkich plików
@@ -21,8 +23,26 @@ function App() {
       ...podcastsData,
     ];
     setVideos(combinedVideos);
-    setLoading(false); // Zakończono ładowanie
   }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedSearchTerm(searchTerm);
+      setLoading(false); // Ustaw loading na false po opóźnieniu
+    }, 2000); // 2-sekundowe opóźnienie
+
+    setLoading(true); // Ustaw loading na true przed rozpoczęciem opóźnienia
+
+    return () => {
+      clearTimeout(timer); // Wyczyść timeout, gdy komponent zostanie odmontowany lub gdy searchTerm się zmienia
+      setLoading(false); // Ustaw loading na false, jeśli timer jest czyszczony
+    };
+  }, [searchTerm]);
+
+  useEffect(() => {
+    // Ustaw loading na false po wybraniu platformy
+    setLoading(false);
+  }, [selectedPlatform]);
 
   const getVideoId = (link) => {
     try {
@@ -46,102 +66,106 @@ function App() {
 
   // Filtrowanie filmów na podstawie wyszukiwania i wybranej platformy
   const filteredVideos = videos.filter(video => {
-    const matchesSearchTerm = video.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchesSearchTerm = video.title.toLowerCase().includes(debouncedSearchTerm.toLowerCase());
     const matchesPlatform = selectedPlatform === 'all' || video.platform === selectedPlatform;
     return matchesSearchTerm && matchesPlatform;
   });
 
-  const handleVideoSelect = (link) => {
-    const videoId = getVideoId(link);
-    setSelectedVideoId(videoId);
+  const handlePlatformChange = (platform) => {
+    setLoading(true);
+    setSelectedPlatform(platform);
+    setTimeout(() => setLoading(false), 1000); // Dodaj krótki czas ładowania przy zmianie platformy
   };
 
   return (
-    <div className="min-h-screen bg-gray-800 p-4 text-gray-200 flex flex-col items-center justify-center">
-      {/* Komunikat ładowania */}
-      {loading ? (
-        <p className="text-lg text-gray-400">Ładowanie linków z YouTube i Rumble...</p>
-      ) : (
-        <>
-          {/* Komponent VideoCount wyświetlany po załadowaniu */}
-          <VideoCount count={videos.length} />
+    <div className="min-h-screen bg-gray-180000 p-4 text-gray-200 flex flex-col items-center justify-center">
+      {/* Komponent VideoCount wyświetlany po załadowaniu */}
+      <VideoCount count={filteredVideos.length} platform={selectedPlatform} />
 
-          {/* Przyciski do wyboru platformy */}
-          <div className="mb-4 flex justify-center">
-            <button
-              onClick={() => setSelectedPlatform('youtube')}
-              className={`mx-2 p-2 rounded ${selectedPlatform === 'youtube' ? 'bg-red-600 text-white' : 'bg-gray-700 text-gray-300'}`}
-            >
-              YouTube
-            </button>
-            <button
-              onClick={() => setSelectedPlatform('rumble')}
-              className={`mx-2 p-2 rounded ${selectedPlatform === 'rumble' ? 'bg-green-600 text-white' : 'bg-gray-700 text-gray-300'}`}
-            >
-              Rumble
-            </button>
-            <button
-              onClick={() => setSelectedPlatform('podcast')}
-              className={`mx-2 p-2 rounded ${selectedPlatform === 'podcast' ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-300'}`}
-            >
-              Podcast
-            </button>
+      {/* Przyciski do wyboru platformy */}
+      <div className="mb-4 flex justify-center">
+        <button
+          onClick={() => handlePlatformChange('youtube')}
+          className={`mx-2 p-2 rounded ${selectedPlatform === 'youtube' ? 'bg-red-600 text-white' : 'bg-gray-700 text-gray-300'}`}
+        >
+          YouTube
+        </button>
+        <button
+          onClick={() => handlePlatformChange('rumble')}
+          className={`mx-2 p-2 rounded ${selectedPlatform === 'rumble' ? 'bg-green-600 text-white' : 'bg-gray-700 text-gray-300'}`}
+        >
+          Rumble
+        </button>
+        <button
+          onClick={() => handlePlatformChange('podcast')}
+          className={`mx-2 p-2 rounded ${selectedPlatform === 'podcast' ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-300'}`}
+        >
+          Podcast
+        </button>
+      </div>
+
+      {/* Pole wyszukiwania */}
+      <div className="mb-4 flex justify-center">
+        <input
+          type="text"
+          placeholder="Szukaj filmów..."
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          className="w-full md:w-2/2 lg:w-2/3 p-3 border border-gray-700 rounded shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+
+      {/* Siatka filmów */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8 flex-grow no-underline  ">
+        {loading && (
+          <div className="col-span-full text-center py-6">
+            <p className="text-lg text-gray-400">Ładowanie...</p>
           </div>
+        )}
 
-          {/* Pole wyszukiwania */}
-          <div className="mb-4 flex justify-center">
-            <input
-              type="text"
-              placeholder="Szukaj filmów..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full md:w-2/2 lg:w-2/3 p-3 border border-gray-700 rounded shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+        {!loading && filteredVideos.length > 0 ? (
+          filteredVideos.map((video, index) => {
+            const videoId = getVideoId(video.link);
+            const isYouTube = video.platform === 'youtube';
+            const isRumble = video.platform === 'rumble';
+            const isPodcast = video.platform === 'podcast';
+            const thumbnailUrl = isYouTube 
+              ? `https://img.youtube.com/vi/${videoId}/hqdefault.jpg` 
+              : isRumble 
+                ? rumbleLogo // Użyj lokalnego logo Rumble
+                : podcastImage; // Użyj lokalnego podcastu
 
-          {/* Siatka filmów */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8 flex-grow no-underline">
-            {filteredVideos.length > 0 ? (
-              filteredVideos.map((video, index) => {
-                const videoId = getVideoId(video.link);
-                const isYouTube = video.platform === 'youtube';
-                const isRumble = video.platform === 'rumble';
-                const isPodcast = video.platform === 'podcast';
-                const thumbnailUrl = isYouTube 
-                  ? `https://img.youtube.com/vi/${videoId}/hqdefault.jpg` 
-                  : isRumble 
-                    ? rumbleLogo // Użyj lokalnego logo Rumble
-                    : 'https://via.placeholder.com/560x315?text=Podcast'; // Placeholder dla podcastów
-
-                return (
-                  <div key={index} className="bg-gray-800 shadow-lg rounded-lg overflow-hidden">
-                    <>
-                      <a href={video.link} target="_blank" rel="noopener noreferrer">
-                        <img 
-                          src={thumbnailUrl} 
-                          alt={video.title} 
-                          className="w-full h-48 object-cover cursor-pointer" 
-                        />
+            return (
+              <div key={index} className="bg-gray-800 shadow-lg rounded-lg overflow-hidden no-underline text-green-500">
+                <>
+                  <a href={video.link} target="_blank" rel="noopener noreferrer">
+                    <img 
+                      src={thumbnailUrl} 
+                      alt={video.title} 
+                      className="w-full h-48 object-cover cursor-pointer" 
+                    />
+                  </a>
+                  <div className="p-4">
+                    <h2 className="text-xl font-semibold mb-2 text-gray-200 no-underline"> {/* Zmieniono kolor na jasny */}
+                      <a className="text-gray-200 " href={video.link} target="_blank" rel="noopener noreferrer">
+                        {video.title}
                       </a>
-                      <div className="p-4">
-                        <h2 className="text-xl font-semibold mb-2 text-gray-200 no-underline"> {/* Zmieniono kolor na jasny */}
-                          <a href={video.link} target="_blank" rel="noopener noreferrer">
-                            {video.title}
-                          </a>
-                        </h2>
-                      </div>
-                    </>
+                    </h2>
                   </div>
-                );
-              })
-            ) : (
-              <div className="col-span-full text-center py-6">
-                <p className="text-lg text-gray-500">Brak wyników do wyświetlenia.</p>
+                </>
               </div>
-            )}
-          </div>
-        </>
-      )}
+            );
+          })
+        ) : (
+          !loading && (
+            <div className="col-span-full text-center py-6">
+              <p className="text-lg text-gray-500">Brak wyników do wyświetlenia.</p>
+            </div>
+          )
+        )}
+      </div>
 
       {/* Odtwarzacz na dole ekranu */}
       {selectedVideoId && (
--- src/VideoCount.jsx
+++ src/VideoCount.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-const VideoCount = ({ count }) => {
+const VideoCount = ({ count, platform }) => {
+  // Determine the display text based on the platform
+  const displayText =
+    platform === 'podcast'
+      ? count === 1
+        ? ' podcast'
+        : ' dostępne podcasty'
+      : platform === 'rumble'
+      ? count === 1
+        ? 'film na Rumble'
+        : 'dostępne filmy na Rumble'
+      : platform === 'youtube'
+      ? count === 1
+        ? 'film na YouTube'
+        : 'dostępne ilmy na YouTube'
+      : platform === 'playlist'
+      ? count === 1
+        ? 'playlistę'
+        : ' dostępne playlisty'
+      : count === 1
+      ? 'film'
+      : 'filmy';
+
   return (
-    <div className="p-6 rounded-lg shadow-lg text-center mb-4 bg-gray-900">
+    <div className="p-6  text-center mb-4">
       <h1 className="text-4xl font-extrabold text-red-400 mb-2">Biblioteka Bart - Audiobooki</h1>
       <h2 className="text-3xl font-bold text-white"> {/* Zmniejszony rozmiar h2 */}
-        {count} {count === 1 ? 'film' : 'filmy'} dostępne
+        {count} {displayText} 
       </h2>
       <p className="text-gray-400 text-sm">Ciesz się swoimi ulubionymi audiobookami!</p>
     </div>
@@ -13,4 +35,3 @@ const VideoCount = ({ count }) => {
 };
 
 export default VideoCount;
-
